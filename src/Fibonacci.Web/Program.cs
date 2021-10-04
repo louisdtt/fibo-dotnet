68d9234e6b7a1a501e67fb0db1aa4d6ccee8d6b4
Louis <louis.dutoitld@gmail.com>
2021-10-04 16:54:37 +0200

    nuget

--- src/Fibonacci.Web/Program.cs
+++ src/Fibonacci.Web/Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/fibonacci", async () => await Fibonacci.Compute.ExecuteAsync(new[] {"44", "44", "44"}));
+
 // app.MapGet("/", () => );
 
 app.Run();
